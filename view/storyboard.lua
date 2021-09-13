e6304fa0917ff00f1430f3937073a840dce5db26
mooracle <yanuar.prakosa@gmail.com>
2021-09-13 17:34:16 +0700

    parallax ground and background scrolling

--- view/storyboard.lua
+++ view/storyboard.lua
@@ -6,6 +6,11 @@ WINDOW_WIDTH = 1280
 VIRTUAL_HEIGHT = 288
 VIRTUAL_WIDTH = 512
 
+-- constant for the background and ground 
+BACKGROUND_SCROLL_SPEED = 50
+LOOP_BACKGROUND = 413
+GROUND_SCROLL_SPEED = 100
+
 function love.load()
     -- loading game assets once
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -13,6 +18,10 @@ function love.load()
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{fullscreen=false, vsync=true, resizable= true})
 
     love.window.setTitle('Fifty Bird')
+
+    -- initial locations for background and ground
+    backgroundScroll = 0
+    groundScroll = 0
 end
 
 function love.resize(w,h)
@@ -25,8 +34,24 @@ function love.draw()
     push:start()
     
     -- background
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(background, -backgroundScroll, 0)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     push:finish()
 end
+
+function love.update(dt)
+    -- scroll background
+    if (backgroundScroll < LOOP_BACKGROUND) then
+        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+    else
+        backgroundScroll = 0
+    end
+    -- scroll ground
+    if (groundScroll < VIRTUAL_WIDTH) then
+        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+    else
+        groundScroll = 0
+    end
+end
+
