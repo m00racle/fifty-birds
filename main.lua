c6ae8620676fe5dd14f799cb2d31e9f68b80608a
mooracle <yanuar.prakosa@gmail.com>
2021-09-16 14:22:10 +0700

    require model.bird module to accommodate Bird class

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- add libraries
 push = require 'libs.push'
-class = require 'libs.class'
+Class = require 'libs.class'
 
 -- add assets resources
 background = love.graphics.newImage('res/background.png')
@@ -9,3 +9,4 @@ ground = love.graphics.newImage('res/ground.png')
 -- add modules
 require 'view.storyboard'
 require 'controller.control'
+require 'model.bird'
