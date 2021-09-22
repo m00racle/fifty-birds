915dc640c696573e695450828343df48a67b2f5f
mooracle <yanuar.prakosa@gmail.com>
2021-09-22 14:32:19 +0700

    add gravity effect on bird to drop

--- model/bird.lua
+++ model/bird.lua
@@ -1,5 +1,7 @@
 -- Class Bird 
 
+-- constant value for gravity
+GRAVITY = 20
 Bird = Class{
     init = function(self, birdImage, posX, posY)
         -- init variables
@@ -8,10 +10,19 @@ Bird = Class{
         self.height = self.image:getHeight()
         self.posX = posX
         self.posY = posY
+        self.dy = 0
     end
 }
 
 function Bird:render()
     -- render the bird
     love.graphics.draw(self.image, self.posX - self.width/2, self.posY - self.height/2)
+end
+
+function Bird:drop(dt)
+    -- model the effect of gravity on bird
+    self.dy = self.dy + GRAVITY * dt
+
+    -- apply current gravity accel to y position
+    self.posY = self.posY + self.dy
 end
