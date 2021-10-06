972286635654e9d34886e35a16ab1095222fa3e8
mooracle <yanuar.prakosa@gmail.com>
2021-10-06 14:20:42 +0700

    add new image resource pipe.png to main

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ Class = require 'libs.class'
 background = love.graphics.newImage('res/background.png')
 ground = love.graphics.newImage('res/ground.png')
 birdPng = love.graphics.newImage('res/bird.png')
+pipePng = love.graphics.newImage('res/pipe.png')
 
 -- add modules
 require 'view.storyboard'
