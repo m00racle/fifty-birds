3d016bc6e8506f5563abf3e1fc0b1beea7471f3b
mooracle <yanuar.prakosa@gmail.com>
2021-10-15 12:49:34 +0700

    move updates to controller from storyboard

--- view/storyboard.lua
+++ view/storyboard.lua
@@ -6,17 +6,8 @@ WINDOW_WIDTH = 1280
 VIRTUAL_HEIGHT = 288
 VIRTUAL_WIDTH = 512
 
--- constant for the background and ground 
-BACKGROUND_SCROLL_SPEED = 103
-LOOP_BACKGROUND = 413
-GROUND_SCROLL_SPEED = 150
-LOOP_GROUND = 1100 - VIRTUAL_WIDTH
-
 -- tables of objects
-local pipes = {}
-
--- timer for pipe spawn
-local spawnTimer = 0
+pipes = {}
 
 function love.load()
     -- loading game assets once
@@ -56,39 +47,4 @@ function love.draw()
     push:finish()
 end
 
-function love.update(dt)
-    -- scroll background
-    if (backgroundScroll < LOOP_BACKGROUND) then
-        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
-    else
-        backgroundScroll = 0
-    end
-    -- scroll ground
-    if (groundScroll < LOOP_GROUND) then
-        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
-    else
-        groundScroll = 0
-    end
-    -- spawn pipes in interval time
-    spawnTimer = spawnTimer + dt
-    if spawnTimer > 2 then
-        -- spawn new pipe and put it into the pipes table
-        table.insert(pipes, Pipe(pipePng, VIRTUAL_WIDTH, VIRTUAL_HEIGHT))
-        -- reset spawnTimer
-        spawnTimer = 0
-    end
-
-    -- bird drop due to gravity
-    bird:drop(dt)
-
-    -- update the swaned pipes scrolls
-    for i, pipe in pairs(pipes) do 
-        pipe:update(dt)
-
-        -- when pipe reach the left side of display delete it to save memory
-        if pipe.x < -pipe.width then
-            table.remove(pipes, i)
-        end
-    end
-end
 
