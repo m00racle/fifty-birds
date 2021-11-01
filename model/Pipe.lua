264065d999be31a6eb49422b06975e4f44bcf17c
mooracle <yanuar.prakosa@gmail.com>
2021-11-01 14:45:20 +0700

    make constant local values for class pipe

--- model/Pipe.lua
+++ model/Pipe.lua
@@ -1,11 +1,13 @@
 -- prepare the local variable 
-local SCROLL_SPEED = -60
+local PIPE_SPEED = 60
+local PIPE_WIDTH = 70
+local PIPE_HEIGHT = 288
 
 Pipe = Class{
     init = function(self, pipeImage, posX, maxY)
         -- init variables
         self.image = pipeImage
-        self.width = self.image:getWidth()
+        self.width = PIPE_WIDTH
         self.x = posX
         -- pos Y should be random between TO DO: find the optimum bottom limit!! the current makes the pipe lips sunk
         self.y = math.random(maxY / 4, maxY - 20)
@@ -19,5 +21,5 @@ end
 
 function Pipe:update(dt)
     -- pipe movement during the frame
-    self.x = self.x + SCROLL_SPEED * dt
+    self.x = self.x - PIPE_SPEED * dt
 end
