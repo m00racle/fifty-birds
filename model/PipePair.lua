f32a8007c7412156da7a5271c19f60725f0153e5
mooracle <yanuar.prakosa@gmail.com>
2021-11-12 16:12:28 +0700

    build initialization for pipe pair

--- model/PipePair.lua
+++ model/PipePair.lua
@@ -1,8 +1,24 @@
 PipePair = Class{
-    init = function(self)
+    init = function(self, pipeImage, maxHeight, gapHeight, startPos)
         -- init variiables
+        self.image = pipeImage
+        self.pipeWidth = self.image:getWidth
+        self.pipeHeight = self.image:getHeight
+        self.x = startPos
+        -- initiate the y random engine
+        self.lastY = 0
+        self.virtualHeight = maxHeight
+        self.gapHeight = gapHeight
+        self.pipeTable = {}
     end
 }
+
+function PipePair:spawn()
+    -- spawn new pipe pairs in the table
+    -- decide the self.y random start position
+    self.y = math.max(-pipeHeight + 10, math.min(self.lastY + math.random(-20, 20), self.virtualHeight - self.gapHeight - self.pipeHeight))
+    -- now we inititate the top and bottom pipe for the pipeTable.
+end
 
 function PipePair:update(dt)
     -- update the movement of the pipe pair
