daec4d4bfed0a4204bdd8cc4ed19ffecee60e08f
mooracle <yanuar.prakosa@gmail.com>
2021-11-12 14:30:31 +0700

    add PipePair.lua file to handle PipePair class

---
+++ model/PipePair.lua
@@ -0,0 +1,13 @@
+PipePair = Class{
+    init = function(self)
+        -- init variiables
+    end
+}
+
+function PipePair:update(dt)
+    -- update the movement of the pipe pair
+end
+
+function PipePair:render()
+    -- render the pipe pairs
+end
