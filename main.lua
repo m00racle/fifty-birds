aa737d2976138f7f8712c8c87be405cd59e4b245
mooracle <yanuar.prakosa@gmail.com>
2022-03-30 15:52:32 +0700

    put table of sfx and bgm and start the bgm

--- main.lua
+++ main.lua
@@ -46,6 +46,21 @@ function love.load()
 
     love.window.setTitle('Fifty Bird')
 
+    -- init audio table
+    sounds = {
+        ['jump'] = love.audio.newSource('res/jump.wav', 'static'),
+        ['explosion'] = love.audio.newSource('res/explosion.wav', 'static'),
+        ['hurt'] = love.audio.newSource('res/hurt.wav', 'static'),
+        ['score'] = love.audio.newSource('res/score.wav', 'static'),
+        ['thick'] = love.audio.newSource('res/thick.wav', 'static'),
+        ['music'] = love.audio.newSource('res/marios_way.mp3', 'static')
+    }
+
+    -- kick off play BGM
+    -- BGM must loop all the way the app is running not just in play state.
+    sounds['music']:setLooping(true)
+    sounds['music']:play()
+
     -- initial locations for background and ground
     backgroundScroll = 0
     groundScroll = 0
